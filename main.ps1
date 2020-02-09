3fb1b618c5294ed212537fc1b759fe7cf91929d6
ismaventuras <ismaelbautista6@gmail.com>
2020-02-09 23:51:37 +0100

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -24,12 +24,13 @@ function UnlockUser {
 }
 function GetADGroups {
     if($userListComboBox.selectedindex -ne -1){
-        $title='AD Groups for {0}' -f $userListComboBox.SelectedValue
+    <#    $title='AD Groups for {0}' -f $userListComboBox.SelectedValue
         RegisterAction  $title
         foreach($group in (Get-ADPrincipalGroupMembership $userListComboBox.selecteditem).name){
             RegisterAction $group
         }
-        $Textbox_Register.AppendText([System.Environment]::Newline)
+        $Textbox_Register.AppendText([System.Environment]::Newline)#>
+        Get-ADPrincipalGroupMembership $userListComboBox.selecteditem | out-gridview
     }
     else {
         [System.Windows.MessageBox]::Show('No user selected.')
@@ -64,6 +65,19 @@ function OpenExplorer{
         [System.Windows.MessageBox]::Show('No user selected.')
     }
 }
+function isUp{
+
+    $online=Test-Connection $computerListComboBox.SelectedValue -Quiet -Count 1
+
+    if($online){
+        $labelConnectivity.Content = 'online'
+        $labelConnectivity.Background = '#32CD32'
+    }
+    else {
+        $labelConnectivity.Content = 'offline'
+        $labelConnectivity.Background = '#e50000'
+    }
+}
 function FillComputerList{
     $computerListComboBox.ItemsSource = get-adcomputer -f * -properties description  |sort -property Description| select description,name 
     $computerListComboBox.DisplayMemberPath="description"
@@ -78,32 +92,41 @@ Add-Type -AssemblyName PresentationFramework
         xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
         Title="AD User tool" Height="343.664" Width="801.541">
-    <TabControl x:Name="TabControlMain" HorizontalAlignment="Left" Height="311" VerticalAlignment="Top" Width="792">
-        <TabItem x:Name="headerUsers" Header="Users">
-            <Grid x:Name="mainGrid" Width="794" Margin="0,0,-8,0">
-                <Label x:Name="userListBoxLabel" Content="Select a user" HorizontalAlignment="Left" Margin="11,19,0,0" VerticalAlignment="Top"/>
-                <ComboBox x:Name="userListComboBox" HorizontalAlignment="Left" Margin="92,23,0,0" VerticalAlignment="Top" Width="120" IsEditable="True">
+        <Grid HorizontalAlignment="Left" Height="344" VerticalAlignment="Top" Width="802">
+        <TabControl x:Name="TabControlMain" HorizontalAlignment="Left" Height="344" VerticalAlignment="Top" Width="802">
+            <TabItem x:Name="headerUsers" Header="Users">
+                <Grid x:Name="mainGrid" Width="794" Margin="0,0,-8,0">
+                    <Label x:Name="userListBoxLabel" Content="Select a user" HorizontalAlignment="Left" Margin="10,5,0,0" VerticalAlignment="Top"/>
+                    <ComboBox x:Name="userListComboBox" HorizontalAlignment="Left" Margin="10,31,0,0" VerticalAlignment="Top" Width="185" IsEditable="True">
 
-                </ComboBox>
-                <Button x:Name="button_UnlockUser" Content="Unlock user" HorizontalAlignment="Left" Margin="12,75,0,0" VerticalAlignment="Top" Width="91" Height="61"/>
-                <Button x:Name="button_ResetPassword" Content="Reset Password" HorizontalAlignment="Left" Margin="108,75,0,0" VerticalAlignment="Top" Width="91" Height="61"/>
-                <Button x:Name="button_GetGroups" Content="Get groups" HorizontalAlignment="Left" Margin="204,75,0,0" VerticalAlignment="Top" Width="91" Height="61"/>
-                <TextBox x:Name="Textbox_Register" HorizontalAlignment="Left" Height="83" Margin="0,200,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="794"/>
+                    </ComboBox>
+                    <Button x:Name="button_UnlockUser" Content="Unlock user" HorizontalAlignment="Left" Margin="10,58,0,0" VerticalAlignment="Top" Width="91" Height="61"/>
+                    <Button x:Name="button_ResetPassword" Content="Reset Password" HorizontalAlignment="Left" Margin="10,119,0,0" VerticalAlignment="Top" Width="91" Height="61"/>
+                    <Button x:Name="button_GetGroups" Content="Get groups" HorizontalAlignment="Left" Margin="106,58,0,0" VerticalAlignment="Top" Width="91" Height="61"/>
+                    <TextBox x:Name="Textbox_Register" HorizontalAlignment="Left" Height="83" Margin="0,202,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="784"/>
 
-            </Grid>
-        </TabItem>
-        <TabItem x:Name="headerComputers" Header="Computers">
-            <Grid x:Name="Grid_computers" Width="794" Margin="0,0,-8,0">
-                <Label x:Name="computerListBoxLabel" Content="Select a computer" HorizontalAlignment="Left" Margin="0,5,0,0" VerticalAlignment="Top"/>
-                <ComboBox x:Name="computerListComboBox" HorizontalAlignment="Left" Margin="10,31,0,0" VerticalAlignment="Top" Width="174" IsEditable="True">
+                </Grid>
+            </TabItem>
+            <TabItem x:Name="headerComputers" Header="Computers">
+                <Grid x:Name="Grid_computers" Width="794" Margin="0,0,-8,0">
+                    <Label x:Name="computerListBoxLabel" Content="Select a computer" HorizontalAlignment="Left" Margin="10,5,0,0" VerticalAlignment="Top"/>
+                    <ComboBox x:Name="computerListComboBox" HorizontalAlignment="Left" Margin="10,31,0,0" VerticalAlignment="Top" Width="366" IsEditable="True">
 
-                </ComboBox>
-                <TextBox x:Name="Textbox_RegisterComputer" HorizontalAlignment="Left" Height="83" Margin="0,200,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="794"/>
-                <Button x:Name="button_OpenExplorer" Content="Open Explorer" HorizontalAlignment="Left" Margin="10,58,0,0" VerticalAlignment="Top" Width="95" Height="42"/>
+                    </ComboBox>
+                    <TextBox x:Name="Textbox_RegisterComputer" HorizontalAlignment="Left" Height="83" Margin="0,191,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="768"/>
+                    <Button x:Name="button_OpenExplorer" Content="Open Explorer" HorizontalAlignment="Left" Margin="10,58,0,0" VerticalAlignment="Top" Width="95" Height="42"/>
+                    <Label x:Name="labelConnectivity" Content="" HorizontalAlignment="Left" Margin="381,31,0,0" VerticalAlignment="Top"/>
+                    <Button x:Name="button_Ping" Content="Ping" HorizontalAlignment="Left" Margin="110,58,0,0" VerticalAlignment="Top" Width="95" Height="42"/>
 
-            </Grid>
-        </TabItem>
-    </TabControl>
+                </Grid>
+            </TabItem>
+            <TabItem x:Name="headerOptions" Header="Options" HorizontalAlignment="Left" Height="20" VerticalAlignment="Top" Width="54">
+                <Grid Background="#FFE5E5E5">
+                    <Button x:Name="button_UpdateLists" Content="Update Lists" HorizontalAlignment="Left" Margin="10,10,0,0" VerticalAlignment="Top" Width="101" Height="43"/>
+                </Grid>
+            </TabItem>
+        </TabControl>
+    </Grid>
 </Window>
 '@
 #Create a XML reader and read XAML
@@ -122,7 +145,14 @@ $button_UnlockUser.Add_Click({ UnlockUser})
 $button_GetGroups.Add_Click({ GetADGroups})
 $button_ResetPassword.Add_Click({ResetPasswordAD})
 $button_OpenExplorer.Add_Click({OpenExplorer})
-
+$button_Ping.Add_Click({isUp})
+$button_UpdateLists.Add_Click({FillUserList    
+    FillComputerList})
+#Ping on computer change
+#$computerListComboBox.Add_SelectionChanged({isUp})
+#Clean connectiviy status
+$computerListComboBox.Add_SelectionChanged({$labelConnectivity.Background = $Null 
+     $labelConnectivity.Content = ''})
 
 
 #Show the dialog
