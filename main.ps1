eb77ecb0a7309fa9b46c32b40802d5c28b303cdd
ismaventuras <ismaelbautista6@gmail.com>
2020-02-16 16:12:06 +0100

    modifiedGui

--- main.ps1
+++ main.ps1
@@ -1,18 +1,33 @@
-<#Global variables#>
-$ou = "OU=ES,DC=EU,DC=DIR,DC=BUNGE,DC=COM"
+<#  Global variables    #>
+$ou = "OU=ES,DC=EU,DC=DIR,DC=domain,DC=COM"
+$SCCMRemotePath= ""
+$psExecPath= ""
 
+<#  Functions #>
 
-function RegisterAction {
-    param($string)
-    $date=get-date -Format '[hh:mm dd:mm:yy]'
-    $Textbox_Register.AppendText([System.Environment]::Newline)
-    $registerString = '{0} {1}' -f $date , $string
-    $Textbox_Register.AppendText($registerString)
+#global
+function creds{
+    $credPath = ".\creds.xml"
+    # check for stored credential
+    if ( Test-Path $credPath ) {
+        #crendetial is stored, load it 
+        $cred = Import-CliXml -Path $credPath
+        $cred
+    } else {
+        # no stored credential: create store, get credential and save it
+        $parent = split-path $credpath -parent
+        if ( -not (test-Path $parent)) {
+            New-Item -ItemType Directory -Force -Path $parent
+        }
+        $cred = (get-credential -Message "Please use an account with admin rights on AD")
+        $cred | Export-CliXml -Path $credPath
+        $cred
+    }
 }
-
+#Users
 function UnlockUser {
     if($userListComboBox.selectedindex -ne -1){
-    Enable-ADAccount $userListComboBox.selecteditem
+    Enable-ADAccount $userListComboBox.selecteditem -Credential creds
     $outString = 'Account has been unlocked for {0}' -f $userListComboBox.selecteditem
     RegisterAction $outString
     }
@@ -28,7 +43,7 @@ function GetADGroups {
             RegisterAction $group
         }
         $Textbox_Register.AppendText([System.Environment]::Newline)#>
-        Get-ADPrincipalGroupMembership $userListComboBox.selecteditem | out-gridview
+        Get-ADPrincipalGroupMembership $userListComboBox.selecteditem -Credential (creds)| out-gridview -title $userListComboBox.selecteditem
     }
     else {
         [System.Windows.MessageBox]::Show('No user selected.')
@@ -36,8 +51,8 @@ function GetADGroups {
 }
 function ResetPasswordAD {
     if($userListComboBox.selectedindex -ne -1){
-    Set-ADAccountPassword -Identity $userListComboBox.selecteditem -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "Bunge2020" -Force) -PassThru -Confirm:$false
-    Set-ADUser -ChangePasswordAtLogon $true -Identity $userListComboBox.selecteditem -Confirm:$false
+    Set-ADAccountPassword -Identity $userListComboBox.selecteditem -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "Bunge2020" -Force) -PassThru -Confirm:$false -Credential creds
+    Set-ADUser -ChangePasswordAtLogon $true -Identity $userListComboBox.selecteditem -Confirm:$false -Credential creds
     UnlockUser
     $outString = 'Password has been reset to default for user {0}' -f $userListComboBox.selecteditem
     RegisterAction $outString
@@ -51,6 +66,7 @@ function FillUserList {
     $userListComboBox.ItemsSource = get-aduser -f * | sort -property Name
     $userListComboBox.DisplayMemberPath="Name"
 }
+#Computers
 function OpenExplorer{
     if($computerListComboBox.selectedindex -ne -1){
         $path = "\\{0}" -f $computerListComboBox.SelectedValue
@@ -60,7 +76,7 @@ function OpenExplorer{
         explorer $path
     }
     else {
-        [System.Windows.MessageBox]::Show('No user selected.')
+        [System.Windows.MessageBox]::Show('No computer selected.')
     }
 }
 function StartSCCMRemote{
@@ -88,6 +104,14 @@ function isUp{
         $labelConnectivity.Background = '#e50000'
     }
 }
+function GetComputerGroups{
+    if($computerListComboBox.selectedindex -ne -1){
+        get-adcomputer $computerListComboBox.SelectedValue | Get-ADPrincipalGroupMembership | Out-GridView -title $computerListComboBox.SelectedValue
+    }
+    else {
+        [System.Windows.MessageBox]::Show('No computer selected.')
+    }
+}
 function FillComputerList{
     $computerListComboBox.ItemsSource = get-adcomputer -f * -properties description  |sort -property Description| select description,name 
     $computerListComboBox.DisplayMemberPath="description"
@@ -95,24 +119,7 @@ function FillComputerList{
 
 }
 
-function creds{
-    $credPath = ".\creds.xml"
-    # check for stored credential
-    if ( Test-Path $credPath ) {
-        #crendetial is stored, load it 
-        $cred = Import-CliXml -Path $credPath
-        $cred
-    } else {
-        # no stored credential: create store, get credential and save it
-        $parent = split-path $credpath -parent
-        if ( -not (test-Path $parent)) {
-            New-Item -ItemType Directory -Force -Path $parent
-        }
-        $cred = get-credential -Message "Please use an account with rights on AD"
-        $cred | Export-CliXml -Path $credPath
-        $cred
-    }
-}
+
 
 #Library to make the GUI work
 Add-Type -AssemblyName PresentationFramework
@@ -121,19 +128,20 @@ Add-Type -AssemblyName PresentationFramework
 <Window x:Name="Window"
         xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
-        Title="AD User tool" Height="343.664" Width="801.541">
-        <Grid HorizontalAlignment="Left" Height="344" VerticalAlignment="Top" Width="802">
-        <TabControl x:Name="TabControlMain" HorizontalAlignment="Left" Height="344" VerticalAlignment="Top" Width="802">
+        Title="Active Directory PS tool" Height="343.664" Width="461.541" ResizeMode="CanMinimize" WindowStartupLocation="CenterScreen">
+        <Grid HorizontalAlignment="Left" Height="344" VerticalAlignment="Top" Width="460">
+        <TabControl x:Name="TabControlMain" HorizontalAlignment="Left" Height="344" VerticalAlignment="Top" Width="461">
             <TabItem x:Name="headerUsers" Header="Users">
                 <Grid x:Name="mainGrid" Width="794" Margin="0,0,-8,0">
                     <Label x:Name="userListBoxLabel" Content="Select a user" HorizontalAlignment="Left" Margin="10,5,0,0" VerticalAlignment="Top"/>
                     <ComboBox x:Name="userListComboBox" HorizontalAlignment="Left" Margin="10,31,0,0" VerticalAlignment="Top" Width="185" IsEditable="True">
 
                     </ComboBox>
-                    <Button x:Name="button_UnlockUser" Content="Unlock user" HorizontalAlignment="Left" Margin="10,58,0,0" VerticalAlignment="Top" Width="91" Height="61"/>
-                    <Button x:Name="button_ResetPassword" Content="Reset Password" HorizontalAlignment="Left" Margin="10,119,0,0" VerticalAlignment="Top" Width="91" Height="61"/>
-                    <Button x:Name="button_GetGroups" Content="Get groups" HorizontalAlignment="Left" Margin="106,58,0,0" VerticalAlignment="Top" Width="91" Height="61"/>
-                    <TextBox x:Name="Textbox_Register" HorizontalAlignment="Left" Height="83" Margin="0,202,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="784"/>
+                    <StackPanel x:Name="stackPanel_Users" HorizontalAlignment="Left" Height="123" Margin="10,73,0,0" VerticalAlignment="Top" Width="360" Orientation="Horizontal">
+                        <Button x:Name="button_GetGroups" Content="Get groups" HorizontalAlignment="Left" VerticalAlignment="Top" Width="91" Height="61"/>
+                        <Button x:Name="button_UnlockUser" Content="Unlock user" HorizontalAlignment="Left" VerticalAlignment="Top" Width="91" Height="61"/>
+                        <Button x:Name="button_ResetPassword" Content="Reset Password" HorizontalAlignment="Left" VerticalAlignment="Top" Width="91" Height="61"/>
+                    </StackPanel>
 
                 </Grid>
             </TabItem>
@@ -143,11 +151,13 @@ Add-Type -AssemblyName PresentationFramework
                     <ComboBox x:Name="computerListComboBox" HorizontalAlignment="Left" Margin="10,31,0,0" VerticalAlignment="Top" Width="366" IsEditable="True">
 
                     </ComboBox>
-                    <TextBox x:Name="Textbox_RegisterComputer" HorizontalAlignment="Left" Height="83" Margin="0,191,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="768"/>
-                    <Button x:Name="button_OpenExplorer" Content="Open Explorer" HorizontalAlignment="Left" Margin="10,88,0,0" VerticalAlignment="Top" Width="95" Height="42"/>
                     <Label x:Name="labelConnectivity" Content="" HorizontalAlignment="Left" Margin="381,31,0,0" VerticalAlignment="Top"/>
-                    <Button x:Name="button_Ping" Content="Ping" HorizontalAlignment="Left" Margin="110,88,0,0" VerticalAlignment="Top" Width="95" Height="42"/>
-                    <Label x:Name="labelSelected" HorizontalAlignment="Left" Margin="10,53,0,0" VerticalAlignment="Top" Height="30" Width="142" FontSize="10"/>
+                    <Label x:Name="labelSelected" Content="Selected:" HorizontalAlignment="Left" Margin="10,53,0,0" VerticalAlignment="Top" Height="30" Width="142" FontSize="10"/>
+                    <StackPanel x:Name="stackPanel_computers" Margin="10,99,412,103" Orientation="Horizontal">
+                        <Button x:Name="button_Ping" Content="Ping" HorizontalAlignment="Left" VerticalAlignment="Top" Width="95" Height="42"/>
+                        <Button x:Name="button_OpenExplorer" Content="Open Explorer" HorizontalAlignment="Left" VerticalAlignment="Top" Width="95" Height="42"/>
+                        <Button x:Name="button_GetComputerGroups" Content="Get Groups" HorizontalAlignment="Left" VerticalAlignment="Top" Width="95" Height="42"/>
+                    </StackPanel>
 
                 </Grid>
             </TabItem>
@@ -177,13 +187,14 @@ $button_GetGroups.Add_Click({ GetADGroups})
 $button_ResetPassword.Add_Click({ResetPasswordAD})
 $button_OpenExplorer.Add_Click({OpenExplorer})
 $button_Ping.Add_Click({isUp})
+$button_GetComputerGroups.Add_Click({GetComputerGroups})
 $button_UpdateLists.Add_Click({FillUserList    
     FillComputerList})
-#Ping on computer change
-#$computerListComboBox.Add_SelectionChanged({isUp})
+<##Ping on computer change
+$computerListComboBox.Add_SelectionChanged({isUp})#>
 #Clean connectiviy status
 $computerListComboBox.Add_SelectionChanged({$labelConnectivity.Background = $Null 
-     $labelConnectivity.Content = ''
+    $labelConnectivity.Content = ''
     $labelSelected.Content=$computerListComboBox.SelectedValue})
 
 
